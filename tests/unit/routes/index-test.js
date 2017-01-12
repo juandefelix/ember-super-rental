cf0a293360c31ca6d4c3003da4f617e587b3ccdb
juandefelix <juandefelix@yahoo.es>
2017-01-12 10:53:28 -0600

    adds rentals and index routes

---
+++ tests/unit/routes/index-test.js
@@ -0,0 +1,13 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:index', 'Unit | Route | index');
+
+test('it exists', function(assert) {
+  let route = this.subject({
+    replaceWith(routeName) {
+      assert.equal(routeName, 'rentals', 'replace with route name rentals');
+    }
+  });
+  // assert.ok(route);
+  route.beforeModel();
+});
