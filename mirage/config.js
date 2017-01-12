53cf4b71350cd315706b63a53108183b30c3b66b
juandefelix <juandefelix@yahoo.es>
2017-01-12 11:47:48 -0600

    add actions to the rentals model

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,4 @@
 export default function() {
-  export default function() {
   this.namespace = '/api';
 
   this.get('/rentals', function() {
@@ -42,27 +41,26 @@ export default function() {
   });
 }
 
-  // These comments are here to help you get started. Feel free to delete them.
+// These comments are here to help you get started. Feel free to delete them.
 
-  /*
-    Config (with defaults).
+/*
+ Config (with defaults).
 
-    Note: these only affect routes defined *after* them!
-  */
+ Note: these only affect routes defined *after* them!
+*/
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+// this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+// this.namespace = '';    // make this `api`, for example, if your API is namespaced
+// this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  /*
-    Shorthand cheatsheet:
+/*
+ Shorthand cheatsheet:
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
+ this.get('/posts');
+ this.post('/posts');
+ this.get('/posts/:id');
+ this.put('/posts/:id'); // or this.patch
+ this.del('/posts/:id');
 
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
-}
+ http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
+*/
