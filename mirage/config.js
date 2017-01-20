3b506bb3c65c2458925cf33773953826ed6291c5
juandefelix <juandefelix@yahoo.es>
2017-01-20 14:35:23 -0600

    complete chapter "Add Nested Routes"

--- mirage/config.js
+++ mirage/config.js
@@ -49,6 +49,11 @@ export default function() {
       return { data: rentals };
     }
   });
+
+  this.get('/rentals/:id', function(db, request) {
+    console.log(request.params)
+    return { data: rentals.find((rental) => request.params.id === rental.id) };
+  });
 }
 
 // These comments are here to help you get started. Feel free to delete them.
